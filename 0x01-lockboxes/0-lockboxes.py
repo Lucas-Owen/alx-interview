0ca2bc9d2c863588446375d18d6075728423e6ae
Lucas Sangura <sanguraowens@gmail.com>
2024-01-12 21:25:54 +0300

    Optimize recursion for trivial case

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -3,14 +3,29 @@
 from functools import reduce
 
 
-def openBox(boxes, box, opened):
-    """Opens a box and uses the keys in that box to open other boxes"""
-    # Open current box
-    opened[box] = True
+def openBox(boxes, box, opened, count=1):
+    """Uses the keys in box to open other boxes"""
     # Use keys in this box to recursively open other boxes
+    to_open = []
+    # Open all boxes and collect keys
     for i in boxes[box]:
         if i < len(boxes) and not opened[i]:
-            openBox(boxes, i, opened)
+            to_open.extend(
+                j for j in boxes[i] if j < len(boxes) and not opened[i])
+            opened[i] = True
+            count += 1
+    # Try to open all unopened boxes
+    for j in to_open:
+        if not opened[j]:
+            opened[j] = True
+            count += 1
+    # Check if all boxes are opened
+    if count == len(boxes):
+        return True
+    for j in to_open:
+        if openBox(boxes, j, opened, count):
+            return True
+    return False
 
 
 def canUnlockAll(boxes):
@@ -18,6 +33,6 @@ def canUnlockAll(boxes):
     if not boxes:
         return True
     # List to keep track of opened boxes. The first one is always open
-    opened = [False] * len(boxes)
+    opened = [True] + [False] * (len(boxes) - 1)
     openBox(boxes, 0, opened)
     return reduce(lambda b1, b2: b1 and b2, opened, True)
