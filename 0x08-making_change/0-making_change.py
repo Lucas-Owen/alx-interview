c9375ef23bfe350f5a67b8fe0451b20e8a5d22d0
Lucas Owen <65005294+Lucas-Owen@users.noreply.github.com>
2024-02-28 15:03:11 +0300

    Remove unnecessary conditionals

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-"""This is makeChange function"""
+"""This module defines the makeChange function"""
 
 
-def makeChange(coins, total, memo=None):
+def makeChange(coins, total):
     """
     Given a list of coins of different values, returns the fewest
     number of coins needed to meet a given amount total
     """
     if total <= 0:
         return 0
-    results = [float('inf')] * (total+1)
+    results = [total+1] * (total+1)
     results[0] = 0
     for i in range(1, len(results)):
         for coin in coins:
             prev = i - coin
-            if total >= prev >= 0:
-                if results[prev] != float('inf'):
-                    if results[prev] + 1 < results[i]:
-                        results[i] = results[prev] + 1
-    return results[total] if results[total] != float('inf') else -1
+            if prev >= 0:
+                if results[prev] + 1 < results[i]:
+                    results[i] = results[prev] + 1
+    return results[total] if results[total] != total+1 else -1
