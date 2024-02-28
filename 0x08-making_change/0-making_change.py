c3395ab1ade63f36c5e4e8cdcb65be5a86b0100e
Lucas Owen <65005294+Lucas-Owen@users.noreply.github.com>
2024-02-28 08:39:28 +0300

    Replace recursive with iterative approach

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -7,19 +7,15 @@ def makeChange(coins, total, memo=None):
     Given a list of coins of different values, returns the fewest
     number of coins needed to meet a given amount total
     """
-    if memo is None:
-        coins = sorted(coins, reverse=True)
-        memo = {coin: 1 for coin in coins}
-    if total == 0:
+    if total <= 0:
         return 0
-    if total in memo:
-        return memo[total]
-    if total < 0:
-        return -1
-    res = []
-    for coin in coins:
-        tmp = makeChange(coins, total - coin, memo) if coin > 0 else -1
-        if tmp >= 0:
-            res.append(tmp + 1)
-    memo[total] = -1 if len(res) == 0 else min(res)
-    return memo[total]
+    results = [float('inf')] * (total+1)
+    results[0] = 0
+    for i in range(1, len(results)):
+        for coin in coins:
+            prev = i - coin
+            if total >= prev >= 0:
+                if results[prev] != float('inf'):
+                    if results[prev] + 1 < results[i]:
+                        results[i] = results[prev] + 1
+    return results[total] if results[total] != float('inf') else -1
